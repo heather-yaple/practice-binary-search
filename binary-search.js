1fc1cceda492120a6b0f277bdd24e1320d315f59
Heather-Yaple <heatherlyaple@gmail.com>
2024-09-11 17:59:53 -0400

    "Final Changes"

--- binary-search.js
+++ binary-search.js
@@ -1,8 +1,15 @@
 function linearSearch (arr, target) {
 
   // Can you solve this in one line?
-
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i; // Return the index of the target value
+    }
+  }
+  return -1; // Return -1 if the target value is not found
 };
+
+
 
 function binarySearch(arr, target) {
 
@@ -22,7 +29,23 @@ function binarySearch(arr, target) {
       // Move the high pointer to midpoint - 1
 
   // Return -1 if the loop exits with overlapping pointers
+  let left = 0;
+  let right = arr.length - 1;
 
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid; // Return the index of the target value
+    }
+    if (arr[mid] < target) {
+      left = mid + 1; // Target is in the right half
+    } else {
+      right = mid - 1; // Target is in the left half
+    }
+  }
+
+  return -1; // Return -1 if the target value is not found
 }
 
 
